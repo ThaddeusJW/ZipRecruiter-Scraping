28bbeb96b4c88559acefccc40d49c7593f75ea7d
ThaddeusJW <138039631+ThaddeusJW@users.noreply.github.com>
2023-08-19 14:08:12 -0400

    Saving all company names, titles, and perks into lists

--- main.py
+++ main.py
@@ -24,8 +24,10 @@ bs = BeautifulSoup(dr.page_source, "html.parser")
 
 companies = bs.find_all("a", {"class":"company_name"})
 jobs = bs.find_all("h2", {"class":"title"})
+perks = bs.find_all("ul", {"class":"perk_items"})
 company_list = []
 job_list = []
+pay_list = []
 
 for job in jobs:
     job_list.append(job.text.strip())
@@ -33,6 +35,22 @@ for job in jobs:
 for company in companies:
     company_list.append(company.text.strip())
 
+for perk in perks:
+    pay = perk.find("li", {"class":"perk_item"})
+    pay_list.append(pay.text.strip())
+
 jobs_dict = zip(company_list, job_list)
 for k, v in jobs_dict:
     print(f"Company: {k}\nRole: {v}\n")
+
+
+class Positions:
+
+        def __init__(self, company, job, perks):
+             self.company = company
+             self.job = job
+             self.perks = perks
+
+job1 = Positions(company_list[0], job_list[0], pay_list[0])
+
+print(job1.company)
